67bbd5612793eaa35997f19e64d1182c46986fd1
ratulchowdhury <ratulchowdhuryofficial@gmail.com>
2025-11-07 20:46:52 +0530

    Add text structure based splitter demo

---
+++ text_structure_based_splitter_demo.py
@@ -0,0 +1,9 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("sample.pdf")
+docs = loader.load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=40)
+chunks = splitter.split_documents(docs)
+print(f"First Page Content",chunks[0].page_content) 
+print(f"First Page Metadata",chunks[0].metadata)
